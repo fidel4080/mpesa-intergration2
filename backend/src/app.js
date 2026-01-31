daf1674432124cb348e44ee8982fbccf7836d249
Fidel Castro <fidelcastro4080@gmail.com>
2026-01-31 18:50:05 +0300

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const stkRoutes = require("./routes/stkRoutes");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../../frontend')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 
 app.use("/api/v1", stkRoutes);
 
---
+++ frontend/index.js
@@ -0,0 +1,4 @@
+const phone = document.getElementById("phone");
+const amount = document.getElementById("amount");
+const submitBtn = document.getElementById("submitBtn");
+
